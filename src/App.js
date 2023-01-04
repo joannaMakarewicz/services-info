92f2c3b3d5ab985afd4d844e4e3dbd666a7618c1
Joanna Makarewicz <joanna.milun@gmail.com>
2023-01-04 14:33:36 +0100

    Content added&components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import "./App.css";
 
+const posts = [
+  {
+    id: 1,
+    image: "/services-info/1.jpg",
+    title: "Pilne: Co to był za dzień!",
+    intro: "Tego świat jeszcze nie widział",
+  },
+  {
+    id: 2,
+    image: "./2.jpg",
+    title: "Wszyscy zazdroszą Polakom!",
+    intro: "Takiego clickbajtowego tytułu jeszcze nikt nie wymyślił",
+  },
+  {
+    id: 3,
+    image: "3.jpg",
+    title: "Adam Małysz zgolił wąs",
+    intro:
+      "Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy",
+  },
+];
+
+function BlogTitle({image, title, intro}) {
+  return (
+    <article>
+      <h2>{title}</h2>
+      <img src={image} alt="Mountain view"/>
+      <p>{intro}</p>
+    </article>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
-    </div>
+    <>
+    <h1>Halooo</h1>
+    <section className="App">
+      {posts.map((elem, index) => (
+        <BlogTitle key={index} image={elem.image} title={elem.title} intro={elem.intro}  />
+      ))}
+    </section>
+    </>
   );
 }
 
